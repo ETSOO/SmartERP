0df49c0558ba283880e56b2f3a96b43210f96554
Garry Xiao <xz@etsoo.com>
2026-09-03 08:17:56 +0800

    admin app infra

--- infra/02-apply.secrets.sh
+++ infra/02-apply.secrets.sh
@@ -24,4 +24,23 @@ microk8s kubectl kustomize infra/overlays/cn/platform
 microk8s kubectl apply -k infra/overlays/cn/platform
 
 # View the logs for pod failure
-microk8s kubectl logs -n smarterp smarterp-platform-7656999848-p42xz
+microk8s kubectl logs -n smarterp smarterp-platform-***
+
+# Add other appplications as needed
+# coreapp as an example
+mkdir -p infra/base/coreapp infra/overlays/cn/coreapp
+
+# Download the files to the base/coreapp
+curl -k -sSL -o infra/base/coreapp/kustomization.yaml https://ghfast.top/https://raw.githubusercontent.com/etsoo/SmartERP/main/infra/base/coreapp/kustomization.yaml
+curl -k -sSL -o infra/base/coreapp/deployment.yaml https://ghfast.top/https://raw.githubusercontent.com/etsoo/SmartERP/main/infra/base/coreapp/deployment.yaml
+curl -k -sSL -o infra/base/coreapp/service.yaml https://ghfast.top/https://raw.githubusercontent.com/etsoo/SmartERP/main/infra/base/coreapp/service.yaml
+curl -k -sSL -o infra/base/coreapp/route.yaml https://ghfast.top/https://raw.githubusercontent.com/etsoo/SmartERP/main/infra/base/coreapp/route.yaml
+curl -k -sSL -o infra/overlays/cn/coreapp/kustomization.yaml https://ghfast.top/https://raw.githubusercontent.com/etsoo/SmartERP/main/infra/overlays/cn/coreapp/kustomization.yaml
+
+touch secrets/coreapp.env.secrets
+vi secrets/coreapp.env.secrets
+
+cp -f secrets/coreapp.env.secrets infra/overlays/cn/coreapp/
+
+microk8s kubectl kustomize infra/overlays/cn/coreapp
+microk8s kubectl apply -k infra/overlays/cn/coreapp
