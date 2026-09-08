3000906a920e79d96c870e29a4cb6db1456a5646
Garry Xiao <xz@etsoo.com>
2026-09-08 21:21:22 +0800

    Fix docker image url

--- infra/04-helm.sh
+++ infra/04-helm.sh
@@ -1,6 +1,8 @@
 # Debug
 # 在 helm 目录下调试模板
 microk8s helm3 template . \
+  -n smarterp \
+  -f "./config/values-cn.yaml" \
   --set-file secrets.shared=/home/smarterp/secrets/shared.env.secrets \
   --set-file secrets.admin=/home/smarterp/secrets/admin.env.secrets \
   --set-file secrets.coreapp=/home/smarterp/secrets/coreapp.env.secrets \
@@ -17,3 +19,6 @@ microk8s kubectl delete -k admin
 # Remove previous release directory, especially during debugging when other users may have created files
 # 移除之前的发布目录，特别是调试阶段，有其他用户创建了文件
 rm -rf /home/***/deploy/releases/1.0.0
+
+# Remove previous release done by Helm
+microk8s helm3 uninstall smarterp -n smarterp
