3e1cd8d48f1c2f7b62a895e5f142d3892d4906b7
Garry Xiao <xz@etsoo.com>
2026-09-08 19:18:07 +0800

    Format secrets

--- infra/04-helm.sh
+++ infra/04-helm.sh
@@ -1,6 +1,14 @@
 # Debug
 # 在 helm 目录下调试模板
-microk8s helm3 template . --debug
+microk8s helm3 template . \
+  --set-file secrets.shared=/home/smarterp/secrets/shared.env.secrets \
+  --set-file secrets.admin=/home/smarterp/secrets/admin.env.secrets \
+  --set-file secrets.coreapp=/home/smarterp/secrets/coreapp.env.secrets \
+  --set-file secrets.crm=/home/smarterp/secrets/crm.env.secrets \
+  --set-file secrets.platform=/home/smarterp/secrets/platform.env.secrets \
+  --set-file secrets.workercenter=/home/smarterp/secrets/workercenter.env.secrets \
+  --set-file secrets.workercms=/home/smarterp/secrets/workercms.env.secrets \
+  --debug
 
 # Remove previous release done by kustomization
 # Example, when current location is /infra/overlays/cn/
